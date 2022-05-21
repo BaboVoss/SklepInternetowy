056785b13259290a153f6b328e753783f494c1b7
Joanna Pawlas <joanna_pawlas@op.pl>
2022-05-21 15:22:59 +0200

    dodanie zmian

--- src/main/java/com/example/sklepinternetowy/service/ProductService.java
+++ src/main/java/com/example/sklepinternetowy/service/ProductService.java
@@ -1,7 +1,36 @@
 package com.example.sklepinternetowy.service;
 
+import com.example.sklepinternetowy.model.Product;
+import com.example.sklepinternetowy.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getProducts(){
+        return productRepository.findAll();
+    }
+
+    public Product getProduct (Long id){
+        return productRepository.findById(id).orElse(null);
+    }
+    public void editProduct(Product product){
+        productRepository.save(product);
+    }
+    public void deleteProduct(Long id){
+        productRepository.deleteById(id);
+    }
+
+
+
+
 }
